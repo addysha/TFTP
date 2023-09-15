73889d7c61d818bf2aa7aa9828e6eb325d836bde
addysha <addysharmap@gmail.com>
2023-09-16 00:00:47 +1200

    added errors

--- TftpServer.java
+++ TftpServer.java
@@ -1,18 +1,39 @@
-import java.net.*;
-import java.io.*;
-import java.util.*;
+    import java.net.*;
+    import java.io.*;
+    import java.util.*;
 
-class TftpServerWorker extends Thread
-{
+    class TftpServerWorker extends Thread {
     private DatagramPacket req;
     private static final byte RRQ = 1;
     private static final byte DATA = 2;
     private static final byte ACK = 3;
     private static final byte ERROR = 4;
+    private static final int MAX_DISK_SIZE = 131072; // Adjust as needed
 
-    private void sendfile(String filename)
-    {
-      try {
+    private void sendfile(String filename) {
+        try {
+            // Check if the file exists
+            File file = new File(filename);
+            if (!file.exists()) {
+                // Send an ERROR packet with error code 1 (File not found)
+                sendError(ERROR, 1, "File not found", req.getAddress(), req.getPort());
+                return;
+            }
+
+            // Check if the file is readable
+            if (!file.canRead()) {
+                // Send an ERROR packet with error code 2 (Access violation)
+                sendError(ERROR, 2, "Access violation", req.getAddress(), req.getPort());
+                return;
+            }
+
+            // Check if the disk is full or allocation exceeded
+            if (file.length() >= MAX_DISK_SIZE) {
+                // Send an ERROR packet with error code 3 (Disk full or allocation exceeded)
+                sendError(ERROR, 3, "Disk full or allocation exceeded", req.getAddress(), req.getPort());
+                return;
+            }
+
             // Open the requested file for reading
             FileInputStream fileInputStream = new FileInputStream(filename);
 
@@ -36,19 +57,27 @@ class TftpServerWorker extends Thread
                 DatagramPacket dataPacket = new DatagramPacket(packetData, packetData.length, req.getAddress(), req.getPort());
                 dataSocket.send(dataPacket);
 
-                // Wait for ACK from the client
-                byte[] ackData = new byte[4];
-                DatagramPacket ackPacket = new DatagramPacket(ackData, ackData.length);
-                dataSocket.receive(ackPacket);
+                // Set a 1-second timeout for ACK reception
+                dataSocket.setSoTimeout(1000);
 
-                // Check if the received ACK is as expected
-                if (ackData[0] != ACK || ackData[1] != (byte) (blockNumber >> 8) || ackData[2] != (byte) blockNumber) {
-                    // If not, retransmit the DATA packet
-                    continue;
+                while (true) {
+                    try {
+                        // Wait for ACK from the client
+                        byte[] ackData = new byte[4];
+                        DatagramPacket ackPacket = new DatagramPacket(ackData, ackData.length);
+                        dataSocket.receive(ackPacket);
+
+                        // Check if the received ACK is as expected
+                        if (ackData[0] == ACK && ackData[1] == (byte) (blockNumber >> 8) && ackData[2] == (byte) blockNumber) {
+                            // Move to the next block
+                            blockNumber++;
+                            break;
+                        }
+                    } catch (SocketTimeoutException e) {
+                        // Handle the timeout, e.g., retransmit the DATA packet
+                        dataSocket.send(dataPacket);
+                    }
                 }
-
-                // Move to the next block
-                blockNumber++;
 
                 // If the bytesRead is less than 512, it's the last block
                 if (bytesRead < 512) {
@@ -64,54 +93,81 @@ class TftpServerWorker extends Thread
         }
     }
 
-    public void run()
+    // Method to send an ERROR packet with the specified error code and message
+    private void sendError(byte errorCode, int errorNumber, String errorMessage, InetAddress clientAddress, int clientPort) {
+        try {
+            // Calculate the length of the error packet
+            byte[] errorMessageBytes = errorMessage.getBytes();
+            int errorPacketLength = 5 + errorMessageBytes.length;
+
+            // Create an ERROR packet
+            byte[] errorData = new byte[errorPacketLength];
+            errorData[0] = ERROR;
+            errorData[1] = (byte) errorNumber;
+            System.arraycopy(errorMessageBytes, 0, errorData, 4, errorMessageBytes.length);
+            errorData[errorPacketLength - 1] = 0; // Null-terminate the error message
+
+            // Create a DatagramPacket with ERROR packet and send to the client
+            DatagramPacket errorPacket = new DatagramPacket(errorData, errorData.length, clientAddress, clientPort);
+            DatagramSocket errorSocket = new DatagramSocket();
+            errorSocket.send(errorPacket);
+            errorSocket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+        public void run()
+        {
+        // Parse the request packet
+            byte[] requestData = req.getData();
+            if (requestData[0] != RRQ) {
+                // Not a RRQ packet, do nothing
+                return;
+            }
+
+            // Extract the filename from the request packet
+            String filename = new String(requestData, 2, req.getLength() - 2); // Subtract 2 for the opcode
+
+            // Call sendfile to send the file to the client
+            sendfile(filename);
+        }
+
+        public TftpServerWorker(DatagramPacket req)
+        {
+        this.req = req;
+        }
+    }
+
+    class TftpServer
     {
-       // Parse the request packet
-        byte[] requestData = req.getData();
-        if (requestData[0] != RRQ) {
-            // Not a RRQ packet, do nothing
+        public void start_server()
+        {
+            try {
+                DatagramSocket ds = new DatagramSocket();
+                System.out.println("TftpServer on port " + ds.getLocalPort());
+
+                for (;;) {
+                    byte[] buf = new byte[1472];
+                    DatagramPacket p = new DatagramPacket(buf, 1472);
+                    ds.receive(p);
+
+                    TftpServerWorker worker = new TftpServerWorker(p);
+                    worker.start();
+                }
+            } catch (Exception e) {
+                System.err.println("Exception: " + e);
+            }
+
             return;
         }
 
-        // Extract the filename from the request packet
-        String filename = new String(requestData, 2, req.getLength() - 2); // Subtract 2 for the opcode
+        
 
-        // Call sendfile to send the file to the client
-        sendfile(filename);
-    }
-
-    public TftpServerWorker(DatagramPacket req)
-    {
-	this.req = req;
-    }
-}
-
-class TftpServer
-{
-    public void start_server()
-    {
-        try {
-            DatagramSocket ds = new DatagramSocket();
-            System.out.println("TftpServer on port " + ds.getLocalPort());
-
-            for (;;) {
-                byte[] buf = new byte[1472];
-                DatagramPacket p = new DatagramPacket(buf, 1472);
-                ds.receive(p);
-
-                TftpServerWorker worker = new TftpServerWorker(p);
-                worker.start();
-            }
-        } catch (Exception e) {
-            System.err.println("Exception: " + e);
+        
+        public static void main(String args[])
+        {
+            TftpServer d = new TftpServer();
+            d.start_server();
         }
-
-        return;
     }
-
-    public static void main(String args[])
-    {
-        TftpServer d = new TftpServer();
-        d.start_server();
-    }
-}
