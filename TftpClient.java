45bd60acb650da3d2e37f5c88f6263e46a8691fd
addysha <addysharmap@gmail.com>
2023-09-22 01:25:07 +1200

    Fully Functional Tftpclient and server

--- TftpClient.java
+++ TftpClient.java
@@ -1,57 +1,167 @@
 import java.net.*;
+import java.util.Arrays;
 import java.io.*;
 
-class TftpClient
-{
+/**
+ * @author Aditya Sharma 
+ * This class represents a TFTP client that can send a Read Request (RRQ) for a file to a TFTP server,
+ * receive the file data, and save it to a local file.
+ */
+class TftpClient {
     private static final byte RRQ = 1;
     private static final byte DATA = 2;
     private static final byte ACK = 3;
     private static final byte ERROR = 4;
 
-    public static void main (String[] args){
-        if (args.length !=3){
-            System.err.println("<Client> <ip> <port> <String>");
+    // Set static Inet ia variable
+    private static InetAddress ia;
+    // set port variable
+    private static int port;
+    // set block number to be 1 
+    private static byte blockNum = 1;
+
+    /**
+     * The main method for the TFTP client.
+     *
+     * @param args Command line arguments. Expects three arguments: <ip> <port> <filename>.
+     */
+    public static void main(String[] args) {
+        if (args.length != 3) {
+            System.err.println("<Client> <ip> <port> <filename>");
             return;
         }
-        try{
-            InetAddress ia = InetAddress.getByName(args[0]);
-            int port = Integer.parseInt(args[1]);
+        try {
+            ia = InetAddress.getByName(args[0]);
+            port = Integer.parseInt(args[1]);
             String filename = args[2].toString();
+            // Create a ByteArrayOutputStream to construct the Read Request (RRQ) packet
             ByteArrayOutputStream output = new ByteArrayOutputStream();
+            // Write the TFTP RRQ packet type (1) to the output stream.
             output.write(RRQ);
+            // Write the requested filename as bytes to the output stream.
             output.write(filename.getBytes());
+            // Convert the output stream to a byte array.
             byte[] buff = new byte[512];
             buff = output.toByteArray();
-
-            System.out.println("Initial Byte " + buff[0]);
-            System.out.println(buff.length);
-            DatagramPacket dp = new DatagramPacket(buff,buff.length,ia,port);
+            
+            // Create a DatagramPacket containing the RRQ packet to send to the server.
+            DatagramPacket dp = new DatagramPacket(buff, buff.length, ia, port);
+            // Create a DatagramSocket for communication with the server.
             DatagramSocket ds = new DatagramSocket();
+            // Set a socket timeout of 1000 milliseconds (1 second) for receiving packets.
+            ds.setSoTimeout(1000);
+            // Send the RRQ packet to the server.
             ds.send(dp);
 
-            File Filereci = new File("Client/", filename);
-            Filereci.createNewFile();
-            FileOutputStream outputFile = new FileOutputStream(Filereci);
+            // Create a new file for writing the received data to rx.
+            File inputFile = new File("Client/" + filename);
+            inputFile.createNewFile();
+            // Create a FileOutputStream for writing data to the file.
+            FileOutputStream outputFile = new FileOutputStream(inputFile);
+            // Receive the initial data packet or error packet from the server.
+            byte[] outputBuffer = DataAcquired(ds);
 
-            for(;;){
-                byte[] outputBuffer = new byte[512];
-                DatagramPacket dpack = new DatagramPacket(outputBuffer,512);
-                ds.receive(dpack);
-                outputBuffer = dpack.getData();
+            // Check if the received packet is a TFTP DATA packet (2).
+            if (outputBuffer[0] == DATA) {
+
+                // Continue receiving data until a packet with less than 514 bytes is received.
+                while (outputBuffer.length == 514) {
+                     // Exclude the TFTP header (2 bytes) and write the data to the output file.
+                    outputBuffer = Arrays.copyOfRange(outputBuffer, 2, outputBuffer.length);
+                    outputFile.write(outputBuffer);
+                    outputFile.flush();
+
+                    // Receive the next data packet from the server.
+                    outputBuffer = DataAcquired(ds);
+                }
+                // --- for buffer length check
+                // System.out.println("Output buffer length was < 514: " + outputBuffer.length);
+                // Exclude the TFTP header (2 bytes) and write the remaining data to the output file.
+                outputBuffer = Arrays.copyOfRange(outputBuffer, 2, outputBuffer.length);
                 outputFile.write(outputBuffer);
                 outputFile.flush();
-                if(dpack.getLength() !=512){
-                    System.out.println("Closed Recieving");
-                    break;
-                }else{
-
-                }
+                // Check if the received packet is a TFTP ERROR packet (4).
+            } else if (outputBuffer[0] == ERROR) {
+                // Extract the error message from the packet (excluding the opcode).
+                String errorMessage = new String(outputBuffer, 1, outputBuffer.length - 1);
+                // Print the error message.
+                System.out.print(errorMessage);
             }
+            // Flush and close the output file.
             outputFile.flush();
             outputFile.close();
-
-        } catch(Exception e){
+            // Close the DatagramSocket.
+            ds.close();
+        } catch (Exception e) {
+            // Handle any exceptions that may occur during the process.
             System.err.println("Error: " + e);
         }
+    }
+
+    /**
+     * Sends an acknowledgment (ACK) packet to the server.
+     *
+     * @param ds The DatagramSocket for sending the ACK packet.
+     */
+        public static void sendAck(DatagramSocket ds) {
+        // Create a ByteArrayOutputStream to construct the ACK packet.
+        ByteArrayOutputStream output = new ByteArrayOutputStream();
+        // Write the TFTP ACK packet type (3) to the output stream
+        output.write(ACK);
+        // Write the current block number to the output stream.
+        output.write(blockNum);
+        // Convert the output stream to a byte array.
+        byte[] buffer = output.toByteArray();
+        // Create a DatagramPacket containing the ACK packet to send to the server.
+        DatagramPacket dp = new DatagramPacket(buffer, buffer.length, ia, port);
+        try {
+            // Send the ACK packet to the server using the DatagramSocket.
+            ds.send(dp);
+        } catch (Exception e) {
+            // Handle any exceptions that may occur during the sending process.
+            System.err.println("Error: " + e);
+        }
+    }
+
+    /**
+     * Receives data from the server.
+     *
+     * @param ds The DatagramSocket for receiving data packets.
+     * @return The received data as a byte array.
+     */
+    private static byte[] DataAcquired(DatagramSocket ds) {
+        // Create a byte array to store received data (maximum size of 514 bytes).
+        byte[] outputBuffer = new byte[514];
+        // Create a DatagramPacket to receive data into the outputBuffer.
+        DatagramPacket dp = new DatagramPacket(outputBuffer, 514);
+        try {
+            // Receive a packet from the server using the DatagramSocket.
+            ds.receive(dp);
+            // Update the InetAddress and port for potential use in subsequent operations.
+            ia = dp.getAddress();
+            port = dp.getPort();
+        } catch (IOException e) {
+            // If an IOException occurs (e.g., timeout), send an acknowledgment (ACK).
+            sendAck(ds);
+        }
+        // Create a byte array to store the received data without extra padding.
+        byte[] allBytes = new byte[dp.getLength()];
+        // Copy the received data from the DatagramPacket into allBytes.
+        byte[] tempBytes = dp.getData();
+        allBytes = Arrays.copyOfRange(tempBytes, 0, dp.getLength());
+        // Print the number of bytes collected.
+        System.out.println("Collected bytes : " + allBytes.length);
+
+        if (allBytes[0] == DATA) {
+            // Check if the received packet is a TFTP DATA packet (2).
+            sendAck(ds); // Send an acknowledgment (ACK) in response to the received data.
+            // Check if the received block number matches the expected block number.
+            if (allBytes[1] != blockNum) {
+                System.out.println("Not matching block number");
+                allBytes = DataAcquired(ds); // Request retransmission of the missing block.
+            }
+            blockNum++;// Increment the expected block number for the next data packet.
+        }
+        return allBytes; // Return the received data.
     }
 }
--- TftpServer.java
+++ TftpServer.java
@@ -1,105 +1,239 @@
 import java.net.*;
 import java.io.*;
-import java.util.*;
 
-class TftpServerWorker extends Thread
-{
+/**
+ * @author Aditya Sharma 
+ * This class represents a worker thread for a TFTP server. It handles incoming TFTP requests,
+ * processes read requests (RRQ), and sends requested files to the client.
+ */
+class TftpServerWorker extends Thread {
+
     private DatagramPacket req;
+
+    // TFTP packet types
     private static final byte RRQ = 1;
     private static final byte DATA = 2;
     private static final byte ACK = 3;
     private static final byte ERROR = 4;
 
-    private void sendfile(String filename)
-    {
-        /*
-         *Open the file using a FileInputStream and send it, one block at a time to the receiver 
-         */
+    //block number
+    private byte blockNum = 1;
+    private byte requesting = 0;
 
-         try{
-            byte[] byteArr = new byte[512];
-            FileInputStream fileStream = new FileInputStream(filename);
-            DatagramSocket ds = new DatagramSocket();
-            InetAddress ia = req.getAddress();
-            int port = req.getPort();
-
-            int byteR = 0;
-            int ByteTransferred = 0;
-            while((byteR=fileStream.read(byteArr)) != -1){
-                DatagramPacket dp = new DatagramPacket(byteArr, byteR,ia,port);
-                if(byteR != 512){
-                    System.out.println("Is not 512");
-                }
+    /**
+     * Sends a packet and waits for an acknowledgment.
+     *
+     * @param ds The DatagramSocket for sending and receiving packets.
+     * @param dp The DatagramPacket to send.
+     */
+    private void SendPkt(DatagramSocket ds, DatagramPacket dp) {
+        // If total connection requests are 5 return off 
+        if (requesting == 5) {
+            return;
+        } else {
+            try {
+                // send Data packet to datasocket
                 ds.send(dp);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            // waits for the acknowledgement 
+            HoldAck(ds, dp);
+        }
+    }
+
+    /**
+     * Holds the ack for waiting 
+     *
+     * @param ds The DatagramSocket for sending and receiving packets.
+     * @param dp The DatagramPacket to send.
+     */
+    private void HoldAck(DatagramSocket ds, DatagramPacket dp) {
+        byte[] BufferAck = new byte[1472];
+        DatagramPacket p = new DatagramPacket(BufferAck, 1472);
+        try {
+            ds.receive(p);
+            requesting = 0; // Reset the number of consecutive timeouts upon successful receipt.
+
+        } catch (IOException e) {
+            System.out.println("Timeout! Nothing received.");
+            requesting++; 
+            if (requesting == 5) {
+                System.out.println("Total attempts are now 5");
+                return; // If 5 consecutive timeouts occur, exit the method.
+            } else {
+                SendPkt(ds, dp);  // Resend the packet when a timeout occurs.
+            }
+        }
+        BufferAck = p.getData(); // Get the received data from the packet.
+
+         // Check if the received packet is not an ACK with the expected block number.
+        if (!(BufferAck[0] == ACK && BufferAck[1] == blockNum)) {
+
+            if (requesting == 5) {
+                return;// If 5 consecutive timeouts occur, exit the method.
+            }
+            // Update the expected block number.
+            blockNum = BufferAck[1];
+            // Resend the packet when the expected ACK is not received.
+            SendPkt(ds, dp);
+
+        } else {
+            // Increment the block number for the next data packet.
+            blockNum++;
+        }
+    }
+
+    /**
+     * Sends a file to the client in TFTP DATA packets.
+     *
+     * @param filename The name of the file to send.
+     */
+    private void sendfile(String filename) {
+        try {
+            // New byte Array of 512
+            byte[] byteArr = new byte[512];
+            // New fileStream object
+            FileInputStream fileStream = new FileInputStream(filename);
+            // New datagramSocket ds
+            DatagramSocket ds = new DatagramSocket();
+            // Set the time out for the ds to 1 second
+            ds.setSoTimeout(1000);
+            // new ia Internet Address calling for address
+            InetAddress ia = req.getAddress();
+            // port variable to get port numb
+            int port = req.getPort();
+            // set byte reader to 0
+            int byteR = 0;
+            // set total bytes sent to 0 
+            int ByteTransferred = 0;
+            
+            // Read a chunk of data from the file into byteArr.
+            // Continue the loop until the end of file or until 5 attempts have been made.
+            while (((byteR = fileStream.read(byteArr)) != -1) && requesting != 5) {
+
+                ByteArrayOutputStream outPut = new ByteArrayOutputStream();
+                outPut.write(DATA); // Write the TFTP DATA packet type (2).
+                outPut.write(blockNum);  // Write the current block number.
+                outPut.write(byteArr); // Write the data from the file.
+                byte[] buffer = outPut.toByteArray(); // set byte buffer to output.toByteArry
+
+                // Create a DatagramPacket to send the data to the client.
+                DatagramPacket dp = new DatagramPacket(buffer, byteR + 2, ia, port);
+                // Send the data packet to the client.
+                SendPkt(ds, dp);
+                // Increment all the total bytes transferred.
                 ByteTransferred += byteR;
             }
+            // If the total bytes transferred is a multiple of 512 and we haven't reached 5 attempts.
+            if ((ByteTransferred % 512 == 0) && requesting != 5) {
+                ByteArrayOutputStream output = new ByteArrayOutputStream();
+                output.write(DATA); // Write the TFTP DATA  acket type (2).
+                output.write(blockNum); // Write the current block number.
+                byte[] buf = output.toByteArray(); // set byte buffer to output.toByteArry
 
-            if(ByteTransferred % 512 ==0){
-                byteArr = new byte[0];
-                DatagramPacket dp = new DatagramPacket(byteArr, byteArr.length,ia, port);
-                ds.send(dp);
-                System.out.println("512 is true");
-            }
-            fileStream.close();
-            System.out.println("Transferring Done");
-         } catch(Exception e){
+                // Create a DatagramPacket to send an empty DATA packet (end of file marker).
+                DatagramPacket dp = new DatagramPacket(buf, buf.length, ia, port);
+                SendPkt(ds, dp);// Send the empty DATA packet to the client.
+            } 
+            fileStream.close(); // Close the input file stream.
+            ds.close(); // Close the DatagramSocket.
+        } catch (Exception e) {
             System.out.println("Error: " + e);
-         }
-         return;
+        }
+        return; // Return from the method.
     }
 
-    public void run()
-    {
+    /**
+     * Runs the TFTP server worker thread.
+     */
+    public void run() {
+        // Check if the received packet is a Read Request (RRQ).
         byte[] buff = req.getData();
-        if(buff[0]==RRQ){
-            System.out.println("RQQ Request");
+        if (buff[0] == RRQ) {
+            System.out.println("RRQ Request");
+            // Extract the requested filename from the packet data.
             String filename = new String(buff, 1, req.getLength() - 1);
             File file = new File(filename);
-            if(file.exists()){
+            // Check if the requested file exists on the server.
+            if (file.exists()) {
+                
                 System.out.println("File Found: " + filename);
+                // If the file exists, send it to the client using the sendfile method.
                 sendfile(filename);
             } else {
-                System.out.println("File not Found");
-            } 
-        }else if (buff[0] == DATA) {
-            System.out.println("Data Request");
+                // If the file doesn't exist, print an error message.
+                System.out.println("Received RRQ file doesn't exist: " + filename);
+                try {
+                    DatagramSocket ds = new DatagramSocket();
+                    InetAddress ia = req.getAddress();
+                    int port = req.getPort();
+
+                    //Create a new String with error message
+                    String errorMessage = "File is not on the server";
+                    ByteArrayOutputStream output = new ByteArrayOutputStream();
+                    output.write(ERROR);// Write the TFTP ERROR packet type (4).
+                    output.write(errorMessage.getBytes());// Write the error message.
+                    buff = output.toByteArray();
+
+                    DatagramPacket dp = new DatagramPacket(buff, buff.length, ia, port);
+                    ds.send(dp);
+                    // Create and send an ERROR packet to the client indicating that the file is not found.
+                } catch (Exception e) {
+                    System.out.println("Error: " + e);
+                }
+            }
         }
-        
     }
 
-    public TftpServerWorker(DatagramPacket req)
-        {
-            this.req = req;
-        }
+    /**
+     * Constructor for TftpServerWorker.
+     *
+     * @param req The DatagramPacket containing the request.
+     */
+    public TftpServerWorker(DatagramPacket req) {
+        this.req = req;
+    }
 }
 
-class TftpServer
-{
-    public void start_server()
-    {
-	try {
-	    DatagramSocket ds = new DatagramSocket(50000);
-	    System.out.println("TftpServer on port " + ds.getLocalPort());
+/**
+ * This class represents a simple TFTP server that listens for incoming requests and
+ * processes them using worker threads.
+ */
+class TftpServer {
+    /**
+     * Starts the TFTP server and listens for incoming requests.
+     */
+    public void start_server() {
+        try {
+            // New Datagram socket ds 
+            DatagramSocket ds = new DatagramSocket(50000);
+            System.out.println("TFTP Server on port " + ds.getLocalPort());
 
-	    for(;;) {
-		byte[] buf = new byte[1472];
-		DatagramPacket p = new DatagramPacket(buf, 1472);
-		ds.receive(p);
-		
-		TftpServerWorker worker = new TftpServerWorker(p);
-		worker.start();
-	    }
-	}
-	catch(Exception e) {
-	    System.err.println("Exception: " + e);
-	}
-
-	return;
+            //Infinite For loop 
+            for (; ; ) {
+                // new byte buffer set to 1472
+                byte[] buf = new byte[1472];
+                DatagramPacket p = new DatagramPacket(buf, 1472);
+                ds.receive(p);
+                // New worker object 
+                TftpServerWorker worker = new TftpServerWorker(p);
+                worker.start();
+            }
+        } catch (Exception e) {
+            System.err.println("Exception: " + e);
+        }
     }
 
-    public static void main(String args[])
-    {
-	TftpServer d = new TftpServer();
-	d.start_server();
+    /**
+     * Main method to start the TFTP server.
+     *
+     * @param args Command line arguments (not used).
+     */
+    public static void main(String args[]) {
+        // new Server object
+        TftpServer server = new TftpServer();
+        //starts server 
+        server.start_server();
     }
 }
